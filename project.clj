875a95969f9c4288533a1de32cf4c85f1d0df926
Edwin Watkeys <edw@poseur.com>
2013-04-29 09:03:26 -0400

    changed project name

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject gauze "0.1.0"
+(defproject org.clojars.edw/gauze "0.1.0"
   :description "A simple library for doing SQL"
-  :url "http://example.com/FIXME"
+  :url "http://github.com/edw/gauze"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
